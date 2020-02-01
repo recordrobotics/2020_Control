0ad6bb6e9bb181fcb1313311292853a45cca5219
Nicholas Armstrong <nwa31104@gmail.com>
2020-01-31 20:21:37 -0500

    old code sucked and was stupid, now it uses a case statement and is as God intended

--- src/main/java/frc/robot/control/XboxJoystick.java
+++ src/main/java/frc/robot/control/XboxJoystick.java
@@ -24,20 +24,23 @@ public class XboxJoystick extends Controller{
     }
 
     public boolean getButtonState (String buttonName){
-        if (buttonName == "x" || buttonName == "X"){
-            return xbox.getXButton();
-        } else if (buttonName == "y" || buttonName == "Y" ) {
-            return xbox.getYButton();
-        } else if (buttonName == "a" || buttonName == "A"){
-            return xbox.getAButton();
-        } else if (buttonName == "b" || buttonName == "B" ) {
-            return xbox.getBButton();
-        } else if (buttonName == "lb" || buttonName == "LB"){
-            return xbox.getBumper(Hand.kLeft);
-        } else if (buttonName == "rb" || buttonName == "RB" ) {
-            return xbox.getBumper(Hand.kRight);
-        } else {
-            return false;
+        buttonName.toUpperCase();
+
+        switch (buttonName){
+            case "X":
+                return xbox.getXButton();
+            case "Y":
+                return xbox.getYButton();
+            case "A":
+                return xbox.getAButton();
+            case "B":
+                return xbox.getBButton();
+            case "LB":
+                return xbox.getBumper(Hand.kLeft);
+            case "RB":
+                return xbox.getBumper(Hand.kRight);
+            default:
+                return false;
         }
 
     }
