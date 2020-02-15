ee512677f479397dbb0d26b6b351c080d55776af
Nicholas Armstrong <nwa31104@gmail.com>
2020-02-14 20:11:45 -0500

    Added support (untested) for AutoTurn, and commands in general, for the Xbox controller
    I added a new command getButton, and made the Xbox controller set up a list of different JoystickButtons corresponding to buttons on the controller. This means you can use whenPressed for A, B, X, Y, LB, and RB. I also added some documentation and descriptions for different XboxJoystick methods

--- src/main/java/frc/robot/control/XboxJoystick.java
+++ src/main/java/frc/robot/control/XboxJoystick.java
@@ -3,14 +3,58 @@ package frc.robot.control;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import frc.robot.RobotMap;
 
 public class XboxJoystick extends Controller{
+    private static final int NUM_BUTTONS = 10;
+
     XboxController xbox;
+    private JoystickButton[] buttons;
 
     public XboxJoystick(){
         xbox = new XboxController(RobotMap.xboxPort);
+        buttons = new JoystickButton[NUM_BUTTONS];
+
+        //instantiate buttons
+		for(int i = 0; i < NUM_BUTTONS; i++){
+			buttons[i] = new JoystickButton(xbox, i);
+		}
     }
+    /**
+     * Gets the requested JoystickButton
+     * @param buttonName The abbreviated name for the button <p> Accepted inputs are X, Y, A, B, LB, and RB <p> Unaccepted inputs will return the A button
+     * @return The requested JoystickButton, or the A button if buttonName wasn't an accepted input
+     */
+    public JoystickButton getButton(String buttonName){
+        buttonName.toUpperCase();
+        int button;
+        switch (buttonName){
+            case "X":
+                button = 3;
+                break;
+            case "Y":
+                button = 4;
+                break;
+            case "A":
+                button = 1;
+                break;
+            case "B":
+                button = 2;
+                break;
+            case "LB":
+                button = 5;
+                break;
+            case "RB":
+                button = 6;
+                break;
+            default:
+                button = 1;
+                break;
+        }
+		return buttons[button];
+    }
+    
     
     public double getXAxis (){
         return xbox.getX(Hand.kLeft);
@@ -23,7 +67,11 @@ public class XboxJoystick extends Controller{
     public double getYAxis (){
         return -0.25*xbox.getY(Hand.kLeft);
     }
-
+    /**
+     * Gets whether or not a button is pressed as a boolean
+     * @param buttonName The common abbreviation for the button <p> Accepted inputs are X, Y, A, B, LB, RB, LT, and RT <p> Non-accepted inputs will return false
+     * @return The boolean value of the button
+     */
     public boolean getButtonState (String buttonName){
         buttonName.toUpperCase();
 
@@ -64,6 +112,11 @@ public class XboxJoystick extends Controller{
         return xbox.getTriggerAxis(Hand.kLeft);
     }
 
+    /**
+     * Gets whether or not the given trigger's axis is over a set cutoff as a boolean
+     * @param triggerName The trigger to check <p> Accepted inputs are LT and RT, unaccepted inputs will return false
+     * @return If it is past the cutoff
+     */
     public boolean getTrigger(String triggerName) {
         triggerName.toUpperCase();
         double cutoff = 0.5;    
