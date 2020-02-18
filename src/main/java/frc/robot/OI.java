849eba9263046f9eabf4a1cfb74ec2d141dd9f39
Nicholas Armstrong <nwa31104@gmail.com>
2020-02-17 19:38:44 -0500

    I did a little more documentation because I have nothing to do and I like doing this

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -81,6 +81,11 @@ public class OI {
   * * * * * * * * * * * * * *
   */
 
+  /**
+   * Gets the state a button of the specific XboxJoystick instance xbox, basically an extension of {@link XboxJoystick#getButtonState()} specifically for the instance xbox
+   * @param buttonName The abbreviation for the button <p> Accepted inputs are A, B, X, Y, LB, RB, LT, RT
+   * @return Whether or not the buttton buttonName is pressed, unaccepted inputs will return false
+   */
   public static boolean getXboxButtonState(String buttonName){
     return xbox.getButtonState(buttonName);
   }
