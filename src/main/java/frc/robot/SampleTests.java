b68e5b9df44efda257f4d07b88bbac40ad842136
rgarg23 <rit.g.garg@gmail.com>
2020-02-20 19:12:02 -0500

    Commented out code that didn't exist. The code builds now.

--- src/main/java/frc/robot/SampleTests.java
+++ src/main/java/frc/robot/SampleTests.java
@@ -1,11 +1,11 @@
-import org.junit.jupiter.api.AfterAll;
+/*import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;*/
 /*
 We (Gavin and I) used this program to try to test JUint. Look it up if you don't know what it is.
 It failed, but feel free to use this document and the internet to install JUnit 5 and get it working properly on your own time.
@@ -14,8 +14,8 @@ We could test code a lot faster, not just now, but possibly for the future Progr
 Thanks - Rit
 */
 public class SampleTests {
-    @Test
+    //@Test
     void test1() {
-        Assertions.assertEquals(7, 7);
+        //Assertions.assertEquals(7, 7);
     }
 }
