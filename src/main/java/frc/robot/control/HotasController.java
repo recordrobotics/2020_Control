f21ebd25eafddcf55dbaffbdc1508b177e2b41c4
Fluxx7 <nwa31104@gmail.com>
2020-10-06 20:30:01 -0400

    Added JavaDoc comments

--- src/main/java/frc/robot/control/HotasController.java
+++ src/main/java/frc/robot/control/HotasController.java
@@ -5,22 +5,35 @@ import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class HotasController extends Controller{
+	//Creates a specific instance of a hotas controller
 	public Joystick hotas;
 	
-
 	public HotasController(){
 		hotas = new Joystick(RobotMap.hotasPort);
 	}
 
+
+	/**
+	 * Get the rotation/twist of the Hotas controller
+	 * @return The amount the Hotas controller has been twisted as a double
+	 */
 	public double getZ(){
 		return hotas.getTwist();
 	}
 
+	/**
+	 * Get how far forward the Hotas controller is
+	 * @return The amount the Hotas controller has been pushed forward as a double
+	 */
 	public double getYAxis(){
 		//it's negative because all the way forward is -1
 		return -hotas.getY(Hand.kRight);
 	}
 
+	/**
+	 * Get how far to the side the Hotas controller is
+	 * @return The amount the Hotas controller has been pushed to the side as a double
+	 */
 	public double getXAxis(){
 		return hotas.getX(Hand.kRight);
 	}
