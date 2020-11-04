b849e6e6e8b0bd2bce76ff1db5759d8d608a9e64
Tim <tim.evdokimov@gmail.com>
2020-11-03 19:17:13 -0500

    Final Commenting

--- src/main/java/frc/robot/subsystems/DriveMonolith.java
+++ src/main/java/frc/robot/subsystems/DriveMonolith.java
@@ -12,36 +12,59 @@ import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.RobotMap;
 
-
-public class DriveMonolith extends DriveTrain {
 /**
-*   Put methods for controlling this subsystem
-*   here. Call these from Commands.
-*/
+ * Drivetrain subclass for Monolith
+ * Controls the wheels and their motors
+ */
+public class DriveMonolith extends DriveTrain {
+
+  /**
+   * Objects representing the motors
+   * @see RobotMap.java for the port numbers 
+   */
   Spark frontRight = new Spark(RobotMap.driveFrontRightPortMonolith);
   Spark backRight = new Spark(RobotMap.driveBackRightPortMonolith);
   Spark frontLeft = new Spark(RobotMap.driveFrontLeftPortMonolith);
   Spark backLeft = new Spark(RobotMap.driveBackLeftPortMonolith);
 
+  /**
+   * @param amount percent output
+   * Set the speed of the left motors
+   */
   public void moveLeftWheels(double amount){
     frontLeft.set(-amount);
     backLeft.set(-amount);
   }
 
-
+  /**
+   * @param amount percent output
+   * Set the speed of the right motors
+   */
   public void moveRightWheels(double amount){
     frontRight.set(amount);
     backRight.set(amount);
   }
 
+  /**
+   * @return value of the encoder
+   * PLACEHOLDER, no enoders on Monolith atm
+   */
   public double getLeftEncoder(){
     return 0.0;
   }
 
+  /**
+   * @return value of the encoder
+   * PLACEHOLDER, no enoders on Monolith atm
+   */
   public double getRightEncoder(){
     return 0.0;
   }
 
+  /**
+   * reset the encoders to 0
+   * PLACEHOLDER, no encoders on Monolith atm
+   */
   public void resetEncoders(){}
 
   public DifferentialDrive getDrive() {return null;}
