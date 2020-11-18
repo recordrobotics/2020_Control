08a68c0de614605af20349a7bdf0ff4a4af5bea7
patatoes1235 <60528673+patatoes1235@users.noreply.github.com>
2020-11-17 20:46:12 -0500

    completed dashboard abstraction

--- src/main/java/frc/robot/subsystems/Dashboard2020.java
+++ src/main/java/frc/robot/subsystems/Dashboard2020.java
@@ -1,41 +1,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.OI;
-/**
-*import frc.robot.commands.*;
-*import frc.robot.OI;
-*/
+import frc.robot.commands.*;
 import frc.robot.Robot;
-import frc.robot.Robot.CurrentRobot;
-import frc.robot.commands.AutoTurn;
-import frc.robot.commands.MoveForward;
-import frc.robot.commands.TiltAcquisition;
-import frc.robot.commands.MoveToRange;
-import frc.robot.commands.TurnToGoal;
 import frc.robot.control.ButtonMap;
 
 public class Dashboard2020 extends Dashboard {
-    private CurrentRobot cRobot;
-    public Dashboard2020(CurrentRobot r){
-        cRobot = r;
-
+    /**
+    * Creates buttons to execute each of the specified commands at the specified values
+    */
+    public Dashboard2020(){
         SmartDashboard.putData("Move 3ft", new MoveForward(24, 0.2));
         SmartDashboard.putData("Turn Left", new AutoTurn(-90));
         SmartDashboard.putData("Turn Right", new AutoTurn(90));
         SmartDashboard.putData("Tilt Acquisition", new TiltAcquisition());
         SmartDashboard.putData("Move to 3ft", new MoveToRange(36));
-        SmartDashboard.putData("Turn to Goal", new TurnToGoal());    
+        SmartDashboard.putData("Turn to Goal", new TurnToGoal());
     }
-
-    /**
-     * displays a certain value on the dashboard under a certain name
-    */
-    public void display2020(){
-    
+    @Override
+    public void periodic() {
         /**
-        *    SmartDashboard.putNumber("Right Encoder", Robot.driveTrain.getRightEncoder());
+        *   SmartDashboard.putNumber("Right Encoder", Robot.driveTrain.getRightEncoder());
         *   SmartDashboard.putNumber("Left Encoder", Robot.driveTrain.getLeftEncoder());
         */
         SmartDashboard.putNumber("Gyro Angle", Robot.gyro.getDeg());
