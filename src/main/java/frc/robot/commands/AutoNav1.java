3ee81d475ac22fa27d63b4547230234e1551577f
patatoes1235 <60528673+patatoes1235@users.noreply.github.com>
2021-01-22 19:33:56 -0500

    added velocity variable

--- src/main/java/frc/robot/commands/AutoNav1.java
+++ src/main/java/frc/robot/commands/AutoNav1.java
@@ -5,16 +5,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoNav1 extends CommandGroup {
     /**start at edge of starting box
      */
-    public AutoNav1() {
-        addSequential(new CircularTrajectory(-1.8, 0.45, 3));
-        addSequential(new CircularTrajectory(2.5, 0.9, 3));
+    public AutoNav1(double v) {
+        double velocity = v;
+        addSequential(new CircularTrajectory(-1.8, 0.45, velocity));
+        addSequential(new CircularTrajectory(2.5, 0.9, velocity));
         addSequential(new CircularTrajectory(0.5, 2*Math.PI));
         addSequential(new CircularTrajectory(-3.6, 0.65, 3));
         addSequential(new CircularTrajectory(-0.5, 1.5*Math.PI));
-        addSequential(new CircularTrajectory(-2.5, 0.4*Math.PI, 3));
+        addSequential(new CircularTrajectory(-2.5, 0.4*Math.PI, velocity));
         addSequential(new CircularTrajectory(-0.6, 1.2*Math.PI));
-        addSequential(new CircularTrajectory(4, Math.PI/4, 4));
-        addSequential(new CircularTrajectory(-5, Math.PI/4, 4));
+        addSequential(new CircularTrajectory(4, Math.PI/4, velocity+1));
+        addSequential(new CircularTrajectory(-5, Math.PI/4, velocity+1));
 
     }
 }
