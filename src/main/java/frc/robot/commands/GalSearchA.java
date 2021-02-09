497e1103d5d74e4da187a5499a8e24c7ff32679e
patatoes1235 <60528673+patatoes1235@users.noreply.github.com>
2021-02-08 21:16:06 -0500

    added acq command for redpath

--- src/main/java/frc/robot/commands/GalSearchA.java
+++ src/main/java/frc/robot/commands/GalSearchA.java
@@ -9,7 +9,7 @@ public class GalSearchA extends CommandGroup {
 
     private double velocity;
     private double initX = 0.5, initY = 2.3;
-    private boolean redPath = false;
+    private boolean redPath = true;
 
     
 
@@ -33,16 +33,22 @@ public class GalSearchA extends CommandGroup {
 
     private void bluePath() {
         addSequential(new MoveForward(132, velocity));
+        addParallel(new PickUpBall(2));
         addSequential(new CircularTrajectory(-0.9, 5*Math.PI/6, velocity));
+        addParallel(new PickUpBall(2));
         addSequential(new CircularTrajectory(0.8, 5*Math.PI/6, velocity));
+        addParallel(new PickUpBall(2));
         addSequential(new CircularTrajectory(-3, Math.PI/3));
     }
 
     private void redPath() {
         addSequential(new CircularTrajectory(-2, Math.PI/12, velocity));
+        addParallel(new PickUpBall(2));
         addSequential(new CircularTrajectory(1.5, Math.PI/2.5, velocity));
+        addParallel(new PickUpBall(3));
         addSequential(new CircularTrajectory(-1.5, Math.PI/4, velocity));
         addSequential(new CircularTrajectory(-0.5, 11*Math.PI/12, velocity));
+        addParallel(new PickUpBall(2));
         addSequential(new CircularTrajectory(1.25, Math.PI/2, velocity));
         addSequential(new CircularTrajectory(1.75, Math.PI/2.75, velocity*1.25));
         addSequential(new CircularTrajectory(-4.5, Math.PI/6, velocity*1.25));
