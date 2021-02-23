5ba62762d734e126755cb2bdc3c56eb5f89097ff
Timevdo <tim.evdokimov@gmail.com>
2021-02-22 19:50:58 -0500

    Flywheel now over CAN again
    Maybe make a CAN/PWM option at some point?

--- src/main/java/frc/robot/subsystems/Flywheel2020.java
+++ src/main/java/frc/robot/subsystems/Flywheel2020.java
@@ -7,15 +7,9 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PWMTalonFX;
-import frc.robot.RobotMap;
 import frc.robot.commands.ControlFlywheel;
 
 public class Flywheel2020 extends Flywheel {
@@ -23,18 +17,17 @@ public class Flywheel2020 extends Flywheel {
      * flywheelMotor Creates a motor object for the flywheel motor.
      * targetVoltage The target voltage of the flywheel.
      */
-    private int falcon500port = 0;
-    private PWMTalonFX flywheelMotor = new PWMTalonFX(falcon500port);
-    
+    private int falcon500port = 12;
+    private WPI_TalonFX flywheelMotor = new WPI_TalonFX(falcon500port);
+
     private double targetVoltage = 11.5;
 
     /**
      * Creates an Object for the flywheel class.
      */
     public Flywheel2020(){
-        flywheelMotor.setInverted(true);
-        //flywheelMotor.enableVoltageCompensation(true);
-        //flywheelMotor.setVoltage(targetVoltage);
+        flywheelMotor.enableVoltageCompensation(true);
+        flywheelMotor.setVoltage(targetVoltage);
 
         //flywheelEncoder = new Encoder();
     }
@@ -55,13 +48,12 @@ public class Flywheel2020 extends Flywheel {
      * @return The flywheel's voltage output.
      */
     public double getVoltage(){
-        //return flywheelMotor.getMotorOutputVoltage();
-        return -1;
+        return flywheelMotor.getMotorOutputVoltage();
     }
 
     public double getVelocity(){
         double convFactor = ((6 * Math.PI * 0.0254)/2048) * 10 * 0.25;
-        return -flywheelMotor.getSpeed();
+        return flywheelMotor.getSensorCollection().getIntegratedSensorVelocity() * convFactor;
     }
 
 }
